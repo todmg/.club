2b5c37ec76de6ffcc6463c25935aff268ace2445
Lio Young <lio@werewolf.design>
2022-02-08 14:34:29 +0100

    initial commit

---
+++ misc/notes.md
@@ -0,0 +1,14 @@
+- artist
+
+  - info
+    - name
+    - "socials"
+    - bio
+  - have releases
+  - have feat. / interviews
+
+- releases
+  - redirect/download link
+  - attributed to artists
+
+* features & interviews
---
+++ pages/[artist].tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import useSWR from "swr";
+import fs from "fs"
+import path from "path"
+export async function getStaticProps({ params: { artist } }: { params: { artist: string }; }) {
+
+    let Artist = require(`../public/data/artist/${artist}.json`);
+
+    return {
+        props: {
+            artist: Artist
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    // console.log(fs.readdirSync(path.join(__dirname, "../../../public/data/artist")))
+    let artists = fs.readdirSync(path.join(__dirname, "../../../public/data/artist"));
+    let paths = artists.map(artist => ({ params: { artist: artist.replace('.json', '') } }))
+    return { paths, fallback: false }
+
+}
+
+const Artist: React.FC<{ artist?: string }> = ({ artist }) => {
+
+    // console.log(artist);
+
+
+
+    return (
+        <div className="content">
+            <div className="artist">
+                <div className="artist-info">
+                    <div className="artist-info-image">
+                        {/* @ts-ignore */}
+                        <h1>{artist.title}</h1>
+                        {/* @ts-ignore */}
+                        <img style={{ width: "10%", height: "auto" }} src={artist.artwork} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Artist;
